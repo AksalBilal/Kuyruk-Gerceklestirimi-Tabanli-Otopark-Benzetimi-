75261c38c7015a7c1a125784aefa8868918bddc4
AksalBilal <bilal.aksal@gmail.com>
2019-02-27 21:03:43 +0300

    Kuyruk-Gerceklestirimi-Tabanli-Otopark-Benzetimi-
    Almış olduğum Veri Yapıları dersi kapsamında kuyruk yapısı kullanarak geliştirmiş olduğum uygulamadır

---
+++ ödev1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ödev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKuyruktanSil = new System.Windows.Forms.Button();
+            this.btnKuyrukSuresi = new System.Windows.Forms.Button();
+            this.btnÖncelikliKuyruktanSil = new System.Windows.Forms.Button();
+            this.listKuyruk = new System.Windows.Forms.ListBox();
+            this.listKuyrukSuresi = new System.Windows.Forms.ListBox();
+            this.ListÖncelikliKuyruk = new System.Windows.Forms.ListBox();
+            this.ListÖncelikliKuyrukSuresi = new System.Windows.Forms.ListBox();
+            this.btnOrtalamaHesapla = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.btnÖncelikliKuyrukSuresi = new System.Windows.Forms.Button();
+            this.listZamanFarkı = new System.Windows.Forms.ListBox();
+            this.btnZamanKazancı = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKuyruktanSil
+            // 
+            this.btnKuyruktanSil.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnKuyruktanSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKuyruktanSil.Location = new System.Drawing.Point(38, 12);
+            this.btnKuyruktanSil.Name = "btnKuyruktanSil";
+            this.btnKuyruktanSil.Size = new System.Drawing.Size(178, 87);
+            this.btnKuyruktanSil.TabIndex = 0;
+            this.btnKuyruktanSil.Text = "KUYRUKTAN SİL";
+            this.btnKuyruktanSil.UseVisualStyleBackColor = false;
+            this.btnKuyruktanSil.Click += new System.EventHandler(this.btnKuyruktanSil_Click);
+            // 
+            // btnKuyrukSuresi
+            // 
+            this.btnKuyrukSuresi.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnKuyrukSuresi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.btnKuyrukSuresi.Location = new System.Drawing.Point(318, 12);
+            this.btnKuyrukSuresi.Name = "btnKuyrukSuresi";
+            this.btnKuyrukSuresi.Size = new System.Drawing.Size(178, 87);
+            this.btnKuyrukSuresi.TabIndex = 1;
+            this.btnKuyrukSuresi.Text = "SÜRELERİ LİSTELE";
+            this.btnKuyrukSuresi.UseVisualStyleBackColor = false;
+            this.btnKuyrukSuresi.Click += new System.EventHandler(this.btnKuyrukSuresi_Click);
+            // 
+            // btnÖncelikliKuyruktanSil
+            // 
+            this.btnÖncelikliKuyruktanSil.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnÖncelikliKuyruktanSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnÖncelikliKuyruktanSil.Location = new System.Drawing.Point(596, 12);
+            this.btnÖncelikliKuyruktanSil.Name = "btnÖncelikliKuyruktanSil";
+            this.btnÖncelikliKuyruktanSil.Size = new System.Drawing.Size(178, 87);
+            this.btnÖncelikliKuyruktanSil.TabIndex = 2;
+            this.btnÖncelikliKuyruktanSil.Text = "ÖNCELİKLİ KUYRUKTAN SİL";
+            this.btnÖncelikliKuyruktanSil.UseVisualStyleBackColor = false;
+            this.btnÖncelikliKuyruktanSil.Click += new System.EventHandler(this.btnÖncelikliKuyruktanSil_Click);
+            // 
+            // listKuyruk
+            // 
+            this.listKuyruk.FormattingEnabled = true;
+            this.listKuyruk.ItemHeight = 16;
+            this.listKuyruk.Location = new System.Drawing.Point(12, 120);
+            this.listKuyruk.Name = "listKuyruk";
+            this.listKuyruk.Size = new System.Drawing.Size(233, 180);
+            this.listKuyruk.TabIndex = 4;
+            // 
+            // listKuyrukSuresi
+            // 
+            this.listKuyrukSuresi.FormattingEnabled = true;
+            this.listKuyrukSuresi.ItemHeight = 16;
+            this.listKuyrukSuresi.Location = new System.Drawing.Point(286, 120);
+            this.listKuyrukSuresi.Name = "listKuyrukSuresi";
+            this.listKuyrukSuresi.Size = new System.Drawing.Size(243, 180);
+            this.listKuyrukSuresi.TabIndex = 5;
+            // 
+            // ListÖncelikliKuyruk
+            // 
+            this.ListÖncelikliKuyruk.FormattingEnabled = true;
+            this.ListÖncelikliKuyruk.ItemHeight = 16;
+            this.ListÖncelikliKuyruk.Location = new System.Drawing.Point(571, 123);
+            this.ListÖncelikliKuyruk.Name = "ListÖncelikliKuyruk";
+            this.ListÖncelikliKuyruk.Size = new System.Drawing.Size(233, 180);
+            this.ListÖncelikliKuyruk.TabIndex = 6;
+            // 
+            // ListÖncelikliKuyrukSuresi
+            // 
+            this.ListÖncelikliKuyrukSuresi.FormattingEnabled = true;
+            this.ListÖncelikliKuyrukSuresi.ItemHeight = 16;
+            this.ListÖncelikliKuyrukSuresi.Location = new System.Drawing.Point(849, 123);
+            this.ListÖncelikliKuyrukSuresi.Name = "ListÖncelikliKuyrukSuresi";
+            this.ListÖncelikliKuyrukSuresi.Size = new System.Drawing.Size(256, 180);
+            this.ListÖncelikliKuyrukSuresi.TabIndex = 7;
+            // 
+            // btnOrtalamaHesapla
+            // 
+            this.btnOrtalamaHesapla.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnOrtalamaHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOrtalamaHesapla.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnOrtalamaHesapla.Location = new System.Drawing.Point(12, 393);
+            this.btnOrtalamaHesapla.Name = "btnOrtalamaHesapla";
+            this.btnOrtalamaHesapla.Size = new System.Drawing.Size(267, 67);
+            this.btnOrtalamaHesapla.TabIndex = 8;
+            this.btnOrtalamaHesapla.Text = "ORTALAMA SÜREYİ HESAPLA";
+            this.btnOrtalamaHesapla.UseVisualStyleBackColor = false;
+            this.btnOrtalamaHesapla.Click += new System.EventHandler(this.btnOrtalamaHesapla_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(359, 416);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(112, 22);
+            this.txt1.TabIndex = 9;
+            // 
+            // btnÖncelikliKuyrukSuresi
+            // 
+            this.btnÖncelikliKuyrukSuresi.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnÖncelikliKuyrukSuresi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnÖncelikliKuyrukSuresi.Location = new System.Drawing.Point(884, 12);
+            this.btnÖncelikliKuyrukSuresi.Name = "btnÖncelikliKuyrukSuresi";
+            this.btnÖncelikliKuyrukSuresi.Size = new System.Drawing.Size(178, 87);
+            this.btnÖncelikliKuyrukSuresi.TabIndex = 12;
+            this.btnÖncelikliKuyrukSuresi.Text = "ÖNCELİKLİ KUYRUK SÜRESİNİ LİSTELE";
+            this.btnÖncelikliKuyrukSuresi.UseVisualStyleBackColor = false;
+            this.btnÖncelikliKuyrukSuresi.Click += new System.EventHandler(this.btnÖncelikliKuyrukSuresi_Click_1);
+            // 
+            // listZamanFarkı
+            // 
+            this.listZamanFarkı.FormattingEnabled = true;
+            this.listZamanFarkı.ItemHeight = 16;
+            this.listZamanFarkı.Location = new System.Drawing.Point(801, 342);
+            this.listZamanFarkı.Name = "listZamanFarkı";
+            this.listZamanFarkı.Size = new System.Drawing.Size(341, 180);
+            this.listZamanFarkı.TabIndex = 13;
+            // 
+            // btnZamanKazancı
+            // 
+            this.btnZamanKazancı.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnZamanKazancı.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnZamanKazancı.Location = new System.Drawing.Point(571, 360);
+            this.btnZamanKazancı.Name = "btnZamanKazancı";
+            this.btnZamanKazancı.Size = new System.Drawing.Size(177, 100);
+            this.btnZamanKazancı.TabIndex = 14;
+            this.btnZamanKazancı.Text = "ARABALARIN TOPLAM SÜRELERİNİ LİSTELE";
+            this.btnZamanKazancı.UseVisualStyleBackColor = false;
+            this.btnZamanKazancı.Click += new System.EventHandler(this.btnZamanKazancı_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(1171, 532);
+            this.Controls.Add(this.btnZamanKazancı);
+            this.Controls.Add(this.listZamanFarkı);
+            this.Controls.Add(this.btnÖncelikliKuyrukSuresi);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnOrtalamaHesapla);
+            this.Controls.Add(this.ListÖncelikliKuyrukSuresi);
+            this.Controls.Add(this.ListÖncelikliKuyruk);
+            this.Controls.Add(this.listKuyrukSuresi);
+            this.Controls.Add(this.listKuyruk);
+            this.Controls.Add(this.btnÖncelikliKuyruktanSil);
+            this.Controls.Add(this.btnKuyrukSuresi);
+            this.Controls.Add(this.btnKuyruktanSil);
+            this.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKuyruktanSil;
+        private System.Windows.Forms.Button btnKuyrukSuresi;
+        private System.Windows.Forms.Button btnÖncelikliKuyruktanSil;
+        private System.Windows.Forms.ListBox listKuyruk;
+        private System.Windows.Forms.ListBox listKuyrukSuresi;
+        private System.Windows.Forms.ListBox ListÖncelikliKuyruk;
+        private System.Windows.Forms.ListBox ListÖncelikliKuyrukSuresi;
+        private System.Windows.Forms.Button btnOrtalamaHesapla;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btnÖncelikliKuyrukSuresi;
+        private System.Windows.Forms.ListBox listZamanFarkı;
+        private System.Windows.Forms.Button btnZamanKazancı;
+    }
+}
+
---
+++ ödev1/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Queue<string> kuyruk = new Queue<string>();//NORMAL SIRALAMA İÇİN KULLANILDI
+        Queue<string> kuyruk2 = new Queue<string>();// ÖNCELİKLİ SIRALAMA İÇİN KULLANILDI
+        Queue<string> kuyruk3 = new Queue<string>();// ÖNCELİKLİ SIRALAMA DA SÜRE KUYRUĞU İÇİN KULLANILDI
+        Queue<string> kuyruk4 = new Queue<string>();// NORMAL SIRALAMA DA SÜRE KUYRUĞU İÇİN KULLANILDI
+        int[] sure = new int[10];//NORMAL SIRALAMADAKİ ARAÇLARIN OTOPARKTAKİ TOPLAM SÜRESİNİ TUTAR
+        int[] sure2 = new int[10];//ÖNCELİKLİ SIRALAMADAKİ ARAÇLARIN OTOPARKTAKİ TOPLAM SÜRESİNİ TUTAR
+        int[] sonuc = new int[10];// NORMAL SIRALAMADAKİ ARACIN ÖNCELİKLİ SIRALAMADAKİ ARAC İLE ARASINDAKİ TOPLAM SÜRE FARKINI TUTAR
+        int[] arabalar = new int[10];// NORMAL SIRALAMADAKİ ARACLARI TUTAR
+        int[] arabalar2 = new int[10];  // ÖNCELİKLİ SIRALAMADAKİ ARAÇLARI TUTAR    
+        int[] numara = new int[10];// NORMAL SIRALAMADAKİ ARACLARIN RANDOM NUMARALARINI TUTAR
+        int[] numara2 = new int[10];// SİLME İŞLEMLERİNDEN SONRA LİSTE GÜNCELLENMESİ İÇİN
+        int[] numara3 = new int[10];// SİLME İŞLEMLERİNDEN SONRA LİSTE GÜNCELLENMESİ İÇİN
+        int[] numara4 = new int[10];// ÖNCELİKLİ SIRALAMADAKİ ARAÇLARIN SIRALI NUMARASINI TUTAR
+
+             
+        int[,] karsılastırma = new int[10,10];
+        int[,] karsılastırma2 = new int[10,10];
+        float ortalamahesapla = 0.2f;
+        int gecici, gecici2, sayi1, sayi2;
+        float gecici3;
+        private void Form1_Load(object sender, EventArgs e)
+        {                
+            Random rastgele = new Random();
+            Random rastgele2 = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                sayi1= Convert.ToInt32(rastgele.Next(50, 150));
+               
+                if (arabalar.Contains(sayi1))
+                {
+                    sayi1= Convert.ToInt32(rastgele.Next(150, 300));
+                }
+                arabalar[i] = sayi1;
+                sayi2 = Convert.ToInt32(rastgele2.Next(10, 50));
+
+                if (numara.Contains(sayi2))
+                {sayi2= Convert.ToInt32(rastgele2.Next(50, 100)); }
+               
+                numara[i] = sayi2;
+                numara3[i] = numara[i];
+             
+            }
+            for (int i = 0; i < arabalar2.Length; i++)
+            {
+                arabalar2[i] = arabalar[i];
+                numara4[i] = numara[i];
+            }
+            for (int i = 0; i < arabalar.Length; i++)
+            {
+                listKuyruk.Items.Add("" + numara[i] + "  nolu arabanın süresi" + "" + "\t" + arabalar[i]);
+            }
+        
+            for (int i = 0; i < 10; i++)
+            {
+                for (int t = 0; t < 10; t++)
+                {
+                    if (arabalar2[t] > arabalar2[i])
+                    {
+
+                        gecici = arabalar2[i];
+
+                        arabalar2[i] = arabalar2[t];
+
+                        arabalar2[t] = gecici;
+
+                        gecici2 = numara4[i];
+
+                        numara4[i] = numara4[t];
+
+                        numara4[t] = gecici2;
+
+                    }
+                }
+            }
+     
+            for (int i = 0; i < arabalar2.Length; i++)
+            {
+                ListÖncelikliKuyruk.Items.Add("" + numara4[i] + "  nolu arabanın süresi " + "" + "\t" + arabalar2[i]);
+            }
+            for (int i = 0; i < arabalar.Length; i++)
+            {
+                kuyruk.Enqueue(Convert.ToString(arabalar[i]));
+                kuyruk2.Enqueue(Convert.ToString(arabalar2[i]));
+                kuyruk3.Enqueue(Convert.ToString(numara4[i]));
+                kuyruk4.Enqueue(Convert.ToString(numara3[i]));
+            }
+            for (int i = 0; i < arabalar2.Length; i++)
+            {
+                if (kuyruk2.Contains(Convert.ToString(arabalar2[i])))
+                {
+                    numara2[i] = numara[i];
+                }            
+            }           
+            for (int j = 0; j < arabalar.Length; j++)
+            {
+                if (j == 0)
+                {
+                    sure[j] = arabalar[j];
+                    sure2[j] = arabalar2[j];
+                }
+                else
+                {
+                    sure[j] = sure[j - 1] + arabalar[j];
+                    sure2[j] = sure2[j - 1] + arabalar2[j];
+                }
+                if (j == 9)
+                {
+                    ortalamahesapla = sure[j] / 10;
+                    
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                karsılastırma[i, 0] = numara4[i];
+                karsılastırma[i, 1] = sure[i];
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                karsılastırma2[i, 0] = numara[i];
+                karsılastırma2[i, 1] = sure2[i];
+            }
+        } 
+        
+        private void btnKuyruktanSil_Click(object sender, EventArgs e)
+        {
+            
+            listKuyruk.Items.Clear();
+            string isim = kuyruk.Dequeue();
+            string isim3 = kuyruk4.Dequeue();           
+            string[] Dizi = kuyruk.ToArray();
+            string[] Dizi3 = kuyruk4.ToArray();
+           
+            for (int i = 0; i < Dizi.Length; i++)
+            {
+               listKuyruk.Items.Add("" +Dizi3[i] + "  nolu arabanın süresi\t " + "" + Dizi[i]);
+            }
+        }
+        private void btnKuyrukSuresi_Click(object sender, EventArgs e)
+        {
+            listKuyrukSuresi.Items.Clear();
+            for (int i = 0; i < sure.Length; i++)
+            {               
+                listKuyrukSuresi.Items.Add("" + numara3[i] + "  nolu arabanın toplam süresi " + "" + sure[i]);
+            }
+        }
+        private void btnZamanKazancı_Click(object sender, EventArgs e)
+        {
+            listZamanFarkı.Items.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (numara[i] == numara4[j])
+                    {
+                        sonuc[i] = karsılastırma[i, 1] - karsılastırma2[j, 1];
+                    }
+                }  
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                gecici3 = sonuc[i] * 100 / sure2[i];
+                listZamanFarkı.Items.Add("" + numara3[i] + "  nolu arabanın zaman kazancı  " + "" + Convert.ToString(sonuc[i]) + "  %" + gecici3);
+
+            }
+        }
+        
+        private void btnÖncelikliKuyruktanSil_Click(object sender, EventArgs e)
+        {
+            ListÖncelikliKuyruk.Items.Clear();
+            string isim2 = kuyruk2.Dequeue();
+            string isim4 = kuyruk3.Dequeue();
+            string[] Dizi2 = kuyruk2.ToArray();
+            string[] Dizi4 = kuyruk3.ToArray();
+            for (int i = 0; i < Dizi2.Length; i++)
+            {
+                ListÖncelikliKuyruk.Items.Add("" + Dizi4[i] + "  nolu arabanın süresi\t " + "" + Dizi2[i]);
+            }
+        }
+        private void btnÖncelikliKuyrukSuresi_Click_1(object sender, EventArgs e)
+        {
+            ListÖncelikliKuyrukSuresi.Items.Clear();
+            for (int i = 0; i < sure2.Length; i++)
+            {
+                ListÖncelikliKuyrukSuresi.Items.Add("" + numara4[i] + "  nolu arabanın toplam süresi  " + "" + sure2[i]);
+            }
+        }
+        private void btnOrtalamaHesapla_Click(object sender, EventArgs e)
+        {
+            txt1.Text = Convert.ToString(Convert.ToDouble(ortalamahesapla));
+            
+        }
+    }
+}
